f088abecf762f2b352123a7ae7b3e0715cbe9a5e
Your Name <jumanahmaheen28@gmail.com>
2025-01-27 21:09:50 +0530

    mongoupdate

--- backend/database.js
+++ backend/database.js
@@ -1,14 +1,15 @@
 const { MongoClient } = require('mongodb');
 
-
-async function connect() {
+async function connectDB() {
   try {
     const client = new MongoClient(process.env.MONGODB_URI);
     await client.connect();
     console.log('Connected to MongoDB!');
+    return client;
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error('MongoDB connection error:', error);
+    throw error;
   }
 }
 
-connect();
+module.exports = { connectDB };
