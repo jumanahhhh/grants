17d272c2da0655fc95f25ccde7b5f0e10440e140
Your Name <jumanahmaheen28@gmail.com>
2025-01-27 21:01:31 +0530

    mongoupdate

--- backend/database.js
+++ backend/database.js
@@ -1,41 +1,14 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Connected to Atlas');
-    } catch (error) {
-        console.error('MongoDB Connection Error:', error);
-        process.exit(1);
-    }
-};
-
-module.exports = connectDB;
+const { MongoClient } = require('mongodb');
 
 
-// const mongoose = require('mongoose');
+async function connect() {
+  try {
+    const client = new MongoClient(process.env.MONGODB_URI);
+    await client.connect();
+    console.log('Connected to MongoDB!');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
 
-// const connectDB = async () => {
-//     try {
-//         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        
-//         console.log('\n----- Database Connection Info -----');
-//         console.log(`Connected to MongoDB Atlas`);
-//         console.log(`Host: ${conn.connection.host}`);
-//         console.log(`Database Name: ${conn.connection.db.databaseName}`);
-        
-//         // List all collections
-//         const collections = await conn.connection.db.listCollections().toArray();
-//         console.log('Collections:', collections.map(c => c.name).join(', '));
-//         console.log('---------------------------------\n');
-
-//     } catch (error) {
-//         console.error('MongoDB Connection Error:', error);
-//         process.exit(1);
-//     }
-// };
-
-// module.exports = connectDB;
+connect();
